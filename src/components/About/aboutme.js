d585ceeac3bb8e83360a713ee7643eaecc9e9bbe
unknown <km_si@ymail.com>
2021-04-23 23:24:11 -0400

    create aboutme page

---
+++ src/components/About/aboutme.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import coverImage from "../../src/cover-image.jpg"
+
+class About extends Component {
+  render() {
+    return(
+      <div><h1>About Page</h1>
+      <h3>Who Am I?</h3>
+      <img src={coverImage} className="my-2" style={{ width: "100%" }} alt="cover" />
+      </div>
+    )
+  }
+}
+
+export default About;
